776de6039bdf1e94f9c235cd4b8eff1b1d0ed5d0
ncinco <32296929+ncinco@users.noreply.github.com>
2023-07-03 15:23:18 +1200

    Update Kafka.cs

--- AstraDB.Token.Rotation.Configuration/Kafka.cs
+++ AstraDB.Token.Rotation.Configuration/Kafka.cs
@@ -2,12 +2,10 @@
 {
     public class Kafka
     {
-        public const string BrokerList = "cluster.playground.cdkt.io:9092";
-        public const string Topic = "my-playground-token-rotation-topic";
+        public const string BrokerList = "token-management.servicebus.windows.net:9093";
+        public const string Topic = "token-rotation";
 
-        public const string Username = "5g7HyylPjq6F51ZO3fq7lp";
-        public const string Password = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiI1ZzdIeXlsUGpxNkY1MVpPM2ZxN2xwIiwib3JnYW5pemF0aW9uSWQiOjc0NDE5LCJ1c2VySWQiOjg2NTcwLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI5MDUxMTc5ZC0zNjRhLTQ5MmQtOWE3OC0yMzQ4MWFhMmY0YjAifX0.THfL3DDZzLD7HzUWUG7I3ct38dPGbP_NTDzK_c2rmog";
-
-        public const string ConsumerGroup = "$group-token-rotation";
+        public const string Username = "$ConnectionString";
+        public const string Password = "Endpoint=sb://token-management.servicebus.windows.net/;SharedAccessKeyName=write-only-policy;SharedAccessKey=t5Dy/diyni2LD5URr8j2aWddhx+JWNrID+AEhPINaIk=;EntityPath=token-rotation";
     }
 }
